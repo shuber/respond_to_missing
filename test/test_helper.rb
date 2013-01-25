72187e240ac19b608c9cbef738351f295c4c07e3
Sean Huber <shuber@einsteinmedical.com>
2013-01-25 09:40:15 -0800

    Simplify paths initialization in test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,3 @@
+$: << File.expand_path('../../lib', __FILE__)
 require 'test/unit'
-
-$:.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-$:.unshift(File.dirname(__FILE__))
 require 'respond_to_missing'
