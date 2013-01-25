9ff2d659f6c4bf068d45489d0bb38551ef9ef2ca
Sean Huber <shuber@einsteinmedical.com>
2013-01-25 09:39:11 -0800

    Ensure respond_to_missing? always returns a boolean

--- lib/respond_to_missing.rb
+++ lib/respond_to_missing.rb
@@ -1,12 +1,12 @@
-# Defines <tt>Object#respond_to_missing?</tt> and patches <tt>Object#respond_to?</tt> unless this functionality has already been implemented (ruby v1.9+)
+# Defines <tt>Object#respond_to_missing?</tt> and patches <tt>Object#respond_to?</tt> unless this functionality has already been implemented (ruby versions 1.9+)
 module RespondToMissing
   autoload :Version, 'respond_to_missing/version'
 
-  def respond_to?(method, include_private = false)
-    super || respond_to_missing?(method, include_private)
+  def respond_to?(method_name, include_private = false)
+    super || !!respond_to_missing?(method_name, include_private)
   end
 
-  def respond_to_missing?(method, include_private)
+  def respond_to_missing?(method_name, include_private)
     false
   end
 end
--- test/respond_to_missing_test.rb
+++ test/respond_to_missing_test.rb
@@ -2,8 +2,14 @@ require File.expand_path('../test_helper', __FILE__)
 
 class RespondToMissingTest < Test::Unit::TestCase
   module Mock
-    def self.respond_to_missing?(method, include_private)
-      method == :testing || super
+    def self.respond_to_missing?(method_name, include_private)
+      method_name == :testing || super
+    end
+  end
+
+  module NonBooleanMock
+    def self.respond_to_missing?(method_name, include_private)
+      'test'
     end
   end
 
@@ -14,5 +20,9 @@ class RespondToMissingTest < Test::Unit::TestCase
   def test_should_pass_failing_respond_to_calls_to_respond_to_missing
     assert Mock.respond_to?(:testing)
     assert !Mock.respond_to?(:undefined_method)
+  end
+
+  def test_should_always_return_a_boolean
+    assert_equal true, NonBooleanMock.respond_to?(:testing)
   end
 end
